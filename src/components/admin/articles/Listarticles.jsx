954b6a1d70c03404a74c79f5871bc45b5bc1df9a
Hachicha Hassan <hsan.hachicha@gmail.com>
2024-07-15 22:18:43 +0200

    V0

--- src/components/admin/articles/Listarticles.jsx
+++ src/components/admin/articles/Listarticles.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetcharticlesPagination } from '../../../services/articleservice'
 
 const Listarticles = () => {
+  const[articles,setArticles]=useState([])
+  const [currentPage,setCurrectPage]=useState(1)
+  const[limit,setLimit]=useState(5)
+  const [totalPages, setTotalPages] = useState(0);
+
+  const fetchProducts=async(page,limit)=>{
+    try {
+      const res=await fetcharticlesPagination(page,limit)
+      setArticles(res.data.products)
+      setTotalPages(res.data.totalPages)
+      console.log(res.data.products)
+      console.log(res.data.totalPages)
+    } catch (error) {
+      alert("erreur de connexion au serveur")
+    }
+  }
+
+useEffect(()=>{
+  fetchProducts(currentPage,limit)
+},[currentPage,limit])
+
   return (
+
+
     <div>
       Liste des articles
     </div>
